3d4379f1ebf0d76828b3566cf8a8aeb795518e3f
PedroSanagiotto <rm93182@fiap.com.br>
2022-10-02 20:53:56 -0300

    Criação do styled e estilização do header e lista

---
+++ RWD/react-app-pet/src/components/Style/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const DivHeader = styled.div `
+    background-color:#222;
+    position: relative;
+    width:100%;
+    height: 80px;
+    h1{text-align: center; background: #fff};
+`
+
+export const DivLista = styled.div`
+width: 100%;
+min-height: 85vh;
+padding: 20px;
+background-color: #ccc;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-around;
+`
