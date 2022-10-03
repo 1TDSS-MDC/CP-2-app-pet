5a303bce24acf3eb08b7122169de919004273a2b
matlencar <rm94642@fiap.com.br>
2022-10-02 23:29:04 -0300

    update index e app

--- RWD/react-app-pet/src/App.jsx
+++ RWD/react-app-pet/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ListaDePets from './components/ListaDePets';
-import { DivApp, DivFooter, DivHeader, DivHome } from './components/Style/styled';
+import { DivFooter, DivHeader, DivHome } from './components/Style/styled';
 
 
 export default function App() {
@@ -9,7 +9,6 @@ export default function App() {
         <DivHome>
             <DivHeader />
 
-            <DivApp />
             <ListaDePets />
             <DivFooter />
         </DivHome>
