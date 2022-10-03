f64685cb79bf954371379a712cb883add8f563a5
RuanSDias <sdias.ruan@gmail.com>
2022-10-02 22:00:16 -0300

    pet cadastrado component

---
+++ RWD/react-app-pet/src/components/PetCadastrado.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DivTarefa } from './style/styled'
+import {FaDog as X } from 'react-icons/fa';
+
+export default function PetCadastrado(props) {
+  return (
+    <DivTarefa>
+      <p>Nome: {props.nome}</p>
+      <p>Idade: {props.idade}</p>
+      <p>Raça: {props.raca}</p>
+      <p>Tamanho: {props.tamanho}</p>
+      <p>Nome do dono: {props.dono}</p>
+      <p>Telefone: {props.telefone}</p>
+      <p>Observação: {props.observacao}</p>
+      <button onClick={props.remove}><X/></button>
+    </DivTarefa>
+  )
+}
